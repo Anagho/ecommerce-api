27d38e573f64f59d93a4333c8f6961623f494ffb
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2022-12-30 10:15:28 +0100

    new changes

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4000
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 require('dotenv/config')
 
-const api = proces.env.API_URL
+const api = process.env.API_URL
 
-app.use('json')
+// middleware
+app.use(express.json())
+app.use(morgan('tiny'))
 
-app.get(`${api}'/products`, (req, res) => {
+// get products
+app.get(`${api}/products`, (req, res) => {
     const product = {
         id: 1,
         name: "hair dresser",
         image: 'some_url'
     }
     res.send(product)
+})
+
+// create product
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct)
+    res.send(newProduct)
+})
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=> {
+    console.log('Database Connection is ready...')
+})
+.catch((err) => {
+    console.log(err)
 })
 
 app.listen(PORT, ()=> {
