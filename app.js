10b3ad4d9d4f81c12bff9622c477c2f02461703d
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2023-01-03 13:43:30 +0100

    added changes

--- app.js
+++ app.js
@@ -3,32 +3,28 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const api = process.env.API_URL
 
 require('dotenv/config')
 
-const api = process.env.API_URL
-
 // middleware
-app.use(express.json())
-app.use(morgan('tiny'))
+app.use(express.json()) // to send json format
+app.use(morgan('tiny')) // to handle events log
 
-// get products
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: "hair dresser",
-        image: 'some_url'
-    }
-    res.send(product)
-})
+// Routes
+const categoriesRoutes = require('./routes/categories')
+const productsRoutes = require('./routes/products')
+const usersRoutes = require('./routes/users')
+const ordersRoutes = require('./routes/orders')
 
-// create product
-app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct)
-    res.send(newProduct)
-})
 
+app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/products`, productsRoutes)
+app.use(`${api}/users`, usersRoutes)
+app.use(`${api}/orders`, ordersRoutes)
+
+
+// Database Connection
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -41,6 +37,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err)
 })
 
+// Server
 app.listen(PORT, ()=> {
     console.log(`server started on port ${PORT}`)
 })
